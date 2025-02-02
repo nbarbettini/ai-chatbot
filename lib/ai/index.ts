151c6eb282d327acc92ac44a3632d29b8e9b4ec2
Nate Barbettini <nate@arcade-ai.com>
2025-02-01 16:10:57 -0800

    Fix syntax

--- lib/ai/index.ts
+++ lib/ai/index.ts
@@ -1,15 +1,18 @@
-import { togetherai, createTogetherAI } from '@ai-sdk/togetherai';
+import { createTogetherAI } from '@ai-sdk/togetherai';
 import { experimental_wrapLanguageModel as wrapLanguageModel } from 'ai';
 
 import { customMiddleware } from './custom-middleware';
 
+const togetherAI = createTogetherAI({
+  apiKey: process.env.TOGETHERAI_API_KEY,
+});
+
 export const customModel = (apiIdentifier: string) => {
   return wrapLanguageModel({
-    model: createTogetherAI({
-      apiKey: process.env.TOGETHERAI_API_KEY,
-    }).languageModel(apiIdentifier),
+    model: togetherAI.languageModel(apiIdentifier),
     middleware: customMiddleware,
   });
 };
 
-export const imageGenerationModel = null; //togetherai.image('dall-e-3');
+const imageModel = togetherAI.imageModel!('black-forest-labs/FLUX.1.1-pro');
+export const imageGenerationModel = imageModel;
